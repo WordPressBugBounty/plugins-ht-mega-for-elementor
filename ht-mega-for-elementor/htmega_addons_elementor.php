febaddb131678302d71359221a29e03d337c9449
Sean Murphy <sean@iamseanmurphy.com>
2026-07-06 09:09:14 +0000

    3.2.0

--- ht-mega-for-elementor/htmega_addons_elementor.php
+++ ht-mega-for-elementor/htmega_addons_elementor.php
@@ -5,7 +5,7 @@
  * Plugin URI: 	https://wphtmega.com/
  * Author: 		HasThemes
  * Author URI: 	https://hasthemes.com/
- * Version: 	3.1.2
+ * Version: 	3.2.0
  * License:     GPL2
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: htmega-addons
@@ -14,7 +14,7 @@
  * Elementor Pro tested up to: 4.1.1
 */
 if( ! defined( 'ABSPATH' ) ) exit(); // Exit if accessed directly
-define( 'HTMEGA_VERSION', '3.1.2' );
+define( 'HTMEGA_VERSION', '3.2.0' );
 define( 'HTMEGA_ADDONS_PL_ROOT', __FILE__ );
 define( 'HTMEGA_ADDONS_PL_URL', plugins_url( '/', HTMEGA_ADDONS_PL_ROOT ) );
 define( 'HTMEGA_ADDONS_PL_PATH', plugin_dir_path( HTMEGA_ADDONS_PL_ROOT ) );
